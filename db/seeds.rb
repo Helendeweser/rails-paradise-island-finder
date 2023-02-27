9c68764954254ab57cb54d3566a86daf50b04f8d
Florian de Radiguès <florian.radigues@gmail.com>
2023-02-27 17:57:07 +0100

    adding 6 islands in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Island.destroy_all
+
+island = Island.new(
+  name: "shutter",
+  location: "England",
+  size: 975,
+  price: 10_764
+)
+island.save
+puts "Creating island #{island.id}"
+
+island = Island.new(
+  name: "Banana Haven",
+  location: "Bahamas",
+  size: 1005,
+  price: 16_764
+)
+island.save
+puts "Creating island #{island.id}"
+
+island = Island.new(
+  name: "Sweetfish Retreat",
+  location: "Groenland",
+  size: 500,
+  price: 4964
+)
+island.save
+puts "Creating island #{island.id}"
+
+island = Island.new(
+  name: "Ilan",
+  location: "Enen",
+  size: 3000,
+  price: 19_646
+)
+island.save
+puts "Creating island #{island.id}"
+
+island = Island.new(
+  name: "Goat",
+  location: "Japan",
+  size: 1930,
+  price: 12_646
+)
+island.save
+puts "Creating island #{island.id}"
+
+island = Island.new(
+  name: "Pedro's",
+  location: "Columbia",
+  size: 300,
+  price: 1046
+)
+island.save
+puts "Creating island #{island.id}"
