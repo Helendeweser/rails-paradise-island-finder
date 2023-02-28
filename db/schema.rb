3f3aa022a7f8dbd9b5d91359ff90680ed836bc49
Marcelo do Nascimento <junior69.mj@gmail.com>
2023-02-28 15:05:22 +0100

    cards done and waiting for images

--- db/schema.rb
+++ db/schema.rb
@@ -35,5 +35,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_110508) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
