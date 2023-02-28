1e450403defe9b4876c248c5d6405062ec866a1a
Helen De Weser <helen.de.weser@gmail.com>
2023-02-28 12:45:25 +0100

    added some methods to the islands controller and a 'new' view

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_153557) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_110833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "islands", force: :cascade do |t|
     t.string "name"
-    t.string "location"
     t.float "size"
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,7 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_153557) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
