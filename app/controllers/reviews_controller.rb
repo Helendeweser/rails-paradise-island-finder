9437b83bed789347af7bb0d7bb0d0433588f3094
Florian de Radiguès <florian.radigues@gmail.com>
2023-02-28 20:59:21 +0100

    controller for reviews done (but empty)

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,7 @@
+class ReviewsController < ApplicationController
+  def create
+    # review = Review.find(params[:id])
+    # island = Island.finf(params[:island_id])
+    # review.island = island
+  end
+end
